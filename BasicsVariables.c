b212c6101fd236d7f234760103abd74226a710ac
guilledmv <45519499+guilledmv@users.noreply.github.com>
2019-07-09 20:01:02 +0200

    Create BasicsVariables.c

---
+++ BasicsVariables.c
@@ -0,0 +1,16 @@
+// We need include the library stdio
+#include <stdio.h>
+// Variables ready for use in C
+
+int main() {
+  int n = 3; // it's a integer value, which is initialized with 3
+  double y = 2.4; // it's a real value, which is initialized with 2.4
+  float x = 4.12; // it's a real value, which is initialized with 4.12
+  // Main diference between double and float is the range of memory
+  char a = 'd'; // it's a character value, which is initialized with d
+  // Besides, we print the numbers in our screen
+  printf("Value of n : %i \n",n);
+  printf("Value of y : %f \n Value of x : %f \n",y,x);
+  printf("Value of a : %c \n",a);
+  return 0;
+  }
